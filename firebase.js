47c6d35a6dd63e38fd30e09174dd8e7ca7167ca4
aminurawayya <aminurawayya2@gmail.com>
2026-06-14 07:12:00 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,7 @@
-// Import Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-storage.js";
 
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
-
-import { getStorage } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-storage.js";
-
-// Your Firebase config
 const firebaseConfig = {
     apiKey: "PASTE_YOURS",
     authDomain: "rawayya-health.firebaseapp.com",
@@ -15,11 +11,7 @@ const firebaseConfig = {
     appId: "PASTE_YOURS"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Firestore Database
 export const db = getFirestore(app);
-
-// Firebase Storage
 export const storage = getStorage(app);
